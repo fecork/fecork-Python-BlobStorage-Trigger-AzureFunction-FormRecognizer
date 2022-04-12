94db5de6af9265aed6f2c449785a420b2c047d7e
fecork <fecork88@gmail.com>
2022-04-12 17:10:48 -0500

    🔧 refactor(limpiar_ficheros): se actualiza el código

--- shared/limpiar_ficheros.py
+++ shared/limpiar_ficheros.py
@@ -4,7 +4,14 @@ import logging
 
 def eliminar_archivos(carpeta):
     lista_archivos = os.listdir(carpeta)
-    for archivo in lista_archivos:
+    formato = ".pdf"
+    lista_busqueda = []
+
+    for valor in lista_archivos:
+        if formato in valor:
+            lista_busqueda.append(valor)
+
+    for archivo in lista_busqueda:
         ruta = f"{carpeta}/{archivo}"
         logging.info(f"Eliminando archivo {ruta}")
         os.unlink(ruta)
--- shared/metodos_pdf.py
+++ shared/metodos_pdf.py
@@ -1,6 +1,5 @@
 import logging
 
-
 from PyPDF2 import PdfFileWriter, PdfFileReader
 
 
