b25c9a2e923d59d3ddb5a05a4188db6023283776
fecork <fecork88@gmail.com>
2022-04-12 08:40:48 -0500

    🔧 refactor(metodos_pdf): se guarda en carpeta temporal los archivos pdf y no en carpetas creadas, esto para poder subirlo a azure function

--- BlobTrigger/__init__.py
+++ BlobTrigger/__init__.py
@@ -3,6 +3,7 @@ import os
 import sys
 
 import azure.functions as func
+import tempfile
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 sys.path.insert(0, dir_path)
@@ -13,23 +14,28 @@ from shared import metodos_pdf
 
 
 def main(myblob: func.InputStream):
+    logging.info("VERSION: 1.0.7 Con Librería TEMP")
     logging.info("====================================================")
     logging.info(
         f"Se detecta nuevo archivo en el blob \n"
         f"Nombre: {myblob.name}\n"
         f"Blob Size: {myblob.length} bytes"
     )
+    logging.info(tempfile.gettempdir())
     logging.info("====================================================")
+    carpeta_temporal = tempfile.gettempdir()
 
-    carpeta_paginas = os.environ["CARPETA_PAGINAS"]
-    carpeta_notificaciones = os.environ["CARPETA_NOTIFICACIONES"]
+    carpeta_paginas = carpeta_temporal
+    carpeta_notificaciones = carpeta_temporal
+    logging.warning(f"carpeta_paginas: {carpeta_paginas}")
+    logging.warning(f"carpeta_notificaciones: {carpeta_notificaciones}")
 
     documento_stream = myblob.read()
     ubicacion_nombre = myblob.name.find("/")
     longitud_nombre = len(myblob.name)
     nombre_documento = myblob.name[ubicacion_nombre + 1 : longitud_nombre]
-    metodos_pdf.obtener_pdf(documento_stream, nombre_documento)
-    metodos_pdf.dividir_pdf(nombre_documento)
+    metodos_pdf.obtener_pdf(documento_stream, nombre_documento, carpeta_notificaciones)
+    metodos_pdf.dividir_pdf(nombre_documento, carpeta_notificaciones, carpeta_paginas)
 
     lista_paginas = os.listdir(carpeta_paginas)
 
--- shared/metodos_pdf.py
+++ shared/metodos_pdf.py
@@ -1,21 +1,19 @@
 import logging
-import os
+
 
 from PyPDF2 import PdfFileWriter, PdfFileReader
 
 
-def obtener_pdf(str64, file_name):
-    carpeta_notificaciones = os.environ["CARPETA_NOTIFICACIONES"]
-    ruta = carpeta_notificaciones + "//" + file_name
+def obtener_pdf(str64, file_name, carpeta_notificaciones):
+    ruta = carpeta_notificaciones + "/" + file_name
     logging.info(f"guardando en {ruta}")
     with open(ruta, "wb") as f:
         f.write(str64)
+    logging.info(f"Archivo guardado en {ruta}")
 
 
-def dividir_pdf(doc_name):
-    carpeta_notificaciones = os.environ["CARPETA_NOTIFICACIONES"]
-    carpeta_paginas = os.environ["CARPETA_PAGINAS"]
-    ruta = carpeta_notificaciones + "//" + str(doc_name)
+def dividir_pdf(doc_name, carpeta_notificaciones, carpeta_paginas):
+    ruta = carpeta_notificaciones + "/" + str(doc_name)
     pdf_reader = PdfFileReader(open(ruta, "rb"))
     page_num = pdf_reader.getNumPages()
 
@@ -26,6 +24,7 @@ def dividir_pdf(doc_name):
         pdf_writer.addPage(pdf_reader.getPage(page))
         extension = "_" + str(page) + ".pdf"
         file_name = doc_name.replace(".pdf", extension)
-        ruta_paginas = carpeta_paginas + "//" + file_name
+        ruta_paginas = carpeta_paginas + "/" + file_name
+
         with open(ruta_paginas, "wb") as f:
             pdf_writer.write(f)
